36341228b6457764d54c5283f8d3b0e85ed942da
ElenaKrylova <ekrylova@usc.edu>
2021-11-02 11:11:18 -0700

    Added flask-login

--- dme_flask/app.py
+++ dme_flask/app.py
@@ -1,4 +1,7 @@
+from re import T
 from flask import Flask, json, jsonify, request, Response
+import flask
+import flask_login
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from datetime import datetime
@@ -6,6 +9,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
+from flask_login import LoginManager, login_user, login_required
 
 import base64
 #from dme_ui_api.api_functions import *
@@ -25,88 +29,132 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/dmedb'
 app.config['JWT_SECRET_KEY'] = 'secret'
 
 mongo_dme = PyMongo(app)
+app.secret_key = b'secret'
+login = LoginManager(app)
+login.login_view = 'login'
 
 CORS(app)
 
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-@app.route('/users/register', methods=["POST"])
-def register():
-    users = mongo.db.users
-    first_name = request.get_json()['first_name']
-    last_name = request.get_json()['last_name']
-    email = request.get_json()['email']
-    password = bcrypt.generate_password_hash(
-        request.get_json()['password']).decode('utf-8')
-    created = datetime.utcnow()
+    @staticmethod
+    def is_authenticated():
+        return True
 
-    exist_user = users.find_one({'email': email})
+    @staticmethod
+    def is_active():
+        return True
 
-    if exist_user:
-        result = Response(
-            '{"error": "Denied: Email already exits."}',
-            status=409,
-            mimetype='application/json'
-        )
-        return result
+    @staticmethod
+    def is_anonymous():
+        return False
 
-    user_id = users.insert({
-        'first_name': first_name,
-        'last_name': last_name,
-        'email': email,
-        'password': password,
-        'created': created
-    })
+    def get_id(self):
+        print("get_id: " + str(self.user_id), flush=True)
+        return self.user_id
 
-    new_user = users.find_one({'_id': user_id})
-
-    result = {'email': new_user['email'] + ' registered'}
-
-    return jsonify({'result': result})
+    @login.user_loader
+    def load_user(user_id):
+        print("user_loader: ", user_id, flush=True)
+        u = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+        if not u:
+            return None
+        return User(user_id=u['_id'])
 
 
-@app.route('/users/login', methods=['POST'])
-def login():
-    users = mongo.db.users
-    email = request.get_json()['email']
-    password = request.get_json()['password']
-    result = ""
+    @login.unauthorized_handler
+    def unauthorized_callback():
+        print("unauthorized", flush=True)
+        return flask.redirect('http://localhost:3000/')
 
-    response = users.find_one({'email': email})
 
-    if response:
-        if bcrypt.check_password_hash(response['password'], password):
-            access_token = create_access_token(identity={
-                'first_name': response['first_name'],
-                'last_name': response['last_name'],
-                'email': response['email']
-            })
-            result = jsonify({'token': access_token})
+    @app.route('/users/register', methods=["POST"])
+    def register():
+        users = mongo.db.users
+        first_name = request.get_json()['first_name']
+        last_name = request.get_json()['last_name']
+        email = request.get_json()['email']
+        password = bcrypt.generate_password_hash(
+            request.get_json()['password']).decode('utf-8')
+        created = datetime.utcnow()
+
+        exist_user = users.find_one({'email': email})
+
+        if exist_user:
+            result = Response(
+                '{"error": "Denied: Email already exits."}',
+                status=409,
+                mimetype='application/json'
+            )
+            return result
+
+        user_id = users.insert({
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': password,
+            'created': created
+        })
+
+        new_user = users.find_one({'_id': user_id})
+
+        result = {'email': new_user['email'] + ' registered'}
+
+        return jsonify({'result': result})
+
+
+    @app.route('/users/login', methods=['POST'])
+    def login():
+        users = mongo.db.users
+        email = request.get_json()['email']
+        password = request.get_json()['password']
+        result = ""
+        
+        response = users.find_one({'email': email})
+
+        if response:
+            if bcrypt.check_password_hash(response['password'], password):
+                access_token = create_access_token(identity={
+                    'first_name': response['first_name'],
+                    'last_name': response['last_name'],
+                    'email': response['email']
+                })
+                user_obj = User(user_id=str(response['_id']))
+                print ("This id was added to User " + str(response['_id']), flush=True)
+                login_user(user_obj, remember=True)
+                result = jsonify({'token': access_token})
+                next = request.args.get('next')
+                if (next):
+                    return flask.redirect(next)
+            else:
+                result = Response(
+                    '{"error": "Unauthorized: Invalid username and password."}',
+                    status=401,
+                    mimetype='application/json'
+                )
         else:
             result = Response(
-                '{"error": "Unauthorized: Invalid username and password."}',
+                '{"error": "Unauthorized: No results found"}',
                 status=401,
                 mimetype='application/json'
             )
-    else:
-        result = Response(
-            '{"error": "Unauthorized: No results found"}',
-            status=401,
-            mimetype='application/json'
-        )
-    return result
+        return result
 
 
 @app.route('/detect', methods=['POST'])
+@login_required
 def search_for_misspell():
     json_data = json.loads(str(request.data, encoding='utf-8'))
     input_str_list = json_data["userInput"].split("\n")
     result_msg = api_misspelling(input_str_list)
-
     result = {"option": result_msg}
     return jsonify(result)
 
 
 @app.route('/model', methods=['POST'])
+@login_required
 def get_result_img():
     json_data = json.loads(str(request.data, encoding='utf-8'))
     input_str_list = json_data["userInput"].split("\n")
@@ -116,13 +164,15 @@ def get_result_img():
 
 
 @app.route('/project', methods=["POST"])
+#@login_required
 def create_project():
     projects = mongo_dme.db.projects
     project_name = request.get_json()['project_name']
     created = datetime.utcnow()
-
+    print(flask_login.current_user, flush =True)
     project_id = projects.insert({
         'project_name': project_name,
+        'created_by': flask_login.current_user.user_id,
         'created': created
     })
 
@@ -132,17 +182,25 @@ def create_project():
 
 
 @app.route('/project/<project_id>', methods=["GET"])
+@login_required
 def get_project(project_id):
     projects = mongo_dme.db.projects
     response = projects.find_one({'_id': ObjectId(project_id)})
     if response:
-        project={
-            'project_id': project_id,
-            'project_name': response['project_name'],
-            'conditions': response['conditions'],
-            'img': response['img']
-        }
-        result = jsonify(project)
+        if response['created_by'] != flask_login.current_user.user_id:
+            result = Response(
+            '{"status_message": "Access Denied: You do not have permissions to access the service."}',
+            status=403,
+            mimetype='application/json'
+        ) 
+        else:           
+            project={
+                'project_id': project_id,
+                'project_name': response['project_name'],
+                'conditions': response['conditions'],
+                'img': response['img']
+            }
+            result = jsonify(project)
     else:
         result = Response(
             '{"status_message": "The resource you requested could not be found."}',
@@ -153,17 +211,25 @@ def get_project(project_id):
 
 
 @app.route('/project/<project_id>', methods=["DELETE"])
+@login_required
 def delete_project(project_id):
     projects = mongo_dme.db.projects
     response = projects.find_one({'_id': ObjectId(project_id)})
     if response:
-        response = projects.remove({'_id': ObjectId(project_id)})
-        if response:
+        if response['created_by'] != flask_login.current_user.user_id:
             result = Response(
-                '{"status_message": "The item was deleted successfully."}',
-                status=200,
-                mimetype='application/json'
-            )
+            '{"status_message": "Access Denied: You do not have permissions to access the service."}',
+            status=403,
+            mimetype='application/json'
+        ) 
+        else:   
+            response = projects.remove({'_id': ObjectId(project_id)})
+            if response:
+                result = Response(
+                    '{"status_message": "The item was deleted successfully."}',
+                    status=200,
+                    mimetype='application/json'
+                )
     else:
         result = Response(
             '{"status_message": "The resource you requested could not be found."}',
@@ -174,23 +240,31 @@ def delete_project(project_id):
 
 
 @app.route('/project/<project_id>', methods=["PUT"])
+@login_required
 def update_project(project_id):
     projects = mongo_dme.db.projects
     response = projects.find_one({'_id': ObjectId(project_id)})
     if response:
-        project_name = request.get_json()['project_name']
-        conditions = request.get_json()['conditions']
-        img = request.get_json()['img']
-        updated = datetime.utcnow()
+        if response['created_by'] != flask_login.current_user.user_id:
+            result = Response(
+            '{"status_message": "Access Denied: You do not have permissions to access the service."}',
+            status=403,
+            mimetype='application/json'
+        ) 
+        else:
+            project_name = request.get_json()['project_name']
+            conditions = request.get_json()['conditions']
+            img = request.get_json()['img']
+            updated = datetime.utcnow()
 
-        projects.update_one({'_id': ObjectId(project_id)}, { '$set': {
-            'project_name': project_name,
-            'conditions': conditions,
-            'img': img,
-            'updated': updated
-        }}, upsert=False)
+            projects.update_one({'_id': ObjectId(project_id)}, { '$set': {
+                'project_name': project_name,
+                'conditions': conditions,
+                'img': img,
+                'updated': updated
+            }}, upsert=False)
 
-        return jsonify({'status_message': project_name + ' was updated successfully.'})
+            return jsonify({'status_message': project_name + ' was updated successfully.'})
     else:
         result = Response(
             '{"status_message": "The resource you requested could not be found."}',
